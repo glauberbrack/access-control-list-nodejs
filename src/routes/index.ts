ef006a0f6ff797d4f803a85d8526139ea2fb0fc2
Glauber Brack <glauber@brack.com.br>
2020-08-12 16:43:54 -0300

    importing sesioncontroller into routes files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import UserController from '../controllers/UserController';
+import SessionController from '../controllers/SessionController';
 
 const routes = Router();
 
 const userController = new UserController();
+const sessionController = new SessionController();
 
-routes.post('/users', userController.store)
+routes.post('/users', userController.store);
+routes.post('/sessions', sessionController.store);
 
 export default routes;
