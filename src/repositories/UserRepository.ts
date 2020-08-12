1411c1160dc054b27a70329fed37c5ae8154c737
Glauber Brack <glauber@brack.com.br>
2020-08-12 00:22:20 -0300

    starting UserRepository

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,10 @@
+import { Repository, EntityRepository } from 'typeorm';
+
+import User from '../models/User';
+
+@EntityRepository(User)
+class UserRepository extends Repository<User> {
+
+}
+
+export default UserRepository;
