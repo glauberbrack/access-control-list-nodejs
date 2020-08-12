71190391cea379db9248352716844b07b8e2eadc
Glauber Brack <glauber@brack.com.br>
2020-08-12 20:28:08 -0300

    Permission  route added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,21 @@ import { Router } from 'express';
 
 import UserController from '../controllers/UserController';
 import SessionController from '../controllers/SessionController';
+import PermissionController from '../controllers/PermissionController';
 
 const routes = Router();
 
 const userController = new UserController();
 const sessionController = new SessionController();
+const permissionController = new PermissionController();
 
+// USERS
 routes.post('/users', userController.store);
+
+// AUTHENTICATION
 routes.post('/sessions', sessionController.store);
+
+// PERMISSIONS
+routes.post('/permissions', permissionController.store);
 
 export default routes;
