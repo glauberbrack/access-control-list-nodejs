b287637856903e44c8cc6fe69100c2e1c2546537
Glauber Brack <glauber@brack.com.br>
2020-08-12 20:26:14 -0300

    Permission controller created

---
+++ src/controllers/PermissionController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import PermissionRepository from '../repositories/PermissionRepository';
+
+
+class PermissionController {
+
+    async store(request: Request, response: Response) {
+        const permissionRepository = getCustomRepository(PermissionRepository);
+
+        const { name, description } = request.body;
+
+        const permissionExists = await permissionRepository.findOne({ name })
+
+        if(permissionExists) {
+            return response.status(400).json({ error: 'Permission already exists.' })
+        }
+
+        const permission = permissionRepository.create({
+            name,
+            description
+        });
+
+        permissionRepository.save(permission);
+
+        return response.status(201).json(permission);
+    }
+}
+
+export default PermissionController;
