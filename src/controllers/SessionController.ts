db317b4905d82b0edb08e52085b177f0f4710e27
Glauber Brack <glauber@brack.com.br>
2020-08-14 15:09:58 -0300

    retuning roles when user log in application

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -12,7 +12,10 @@ class SessionController {
 
         const userRepository = getCustomRepository(UserRepository);
 
-        const user = await userRepository.findOne({ username })
+        const user = await userRepository.findOne(
+            { username },
+            { relations: ['roles'] }
+        )
 
         if(!user) {
             return response.status(400).json({ error: 'Verify your credentials.' })
@@ -24,7 +27,11 @@ class SessionController {
             return response.status(400).json({ error: 'Verify your credentials '});
         };
 
-        const token = sign({}, '87b9ee9a5ad501dc7433575162a89e8c', {
+        const roles = user.roles.map((role) => role.name);
+
+        const token = sign(
+            { roles },
+            '87b9ee9a5ad501dc7433575162a89e8c', {
             subject: user.id,
             expiresIn: '1d'
         });
