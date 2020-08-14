50f339fb7e7085f35933130d81947ccc4918734f
Glauber Brack <glauber@brack.com.br>
2020-08-14 12:11:59 -0300

    adding roles to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import UserController from '../controllers/UserController';
 import SessionController from '../controllers/SessionController';
 import PermissionController from '../controllers/PermissionController';
+import RoleController from '../controllers/RoleController';
 
 const routes = Router();
 
 const userController = new UserController();
 const sessionController = new SessionController();
 const permissionController = new PermissionController();
+const roleController = new RoleController();
 
 // USERS
 routes.post('/users', userController.store);
@@ -18,5 +20,8 @@ routes.post('/sessions', sessionController.store);
 
 // PERMISSIONS
 routes.post('/permissions', permissionController.store);
+
+// ROLES
+routes.post('/roles', roleController.store);
 
 export default routes;
