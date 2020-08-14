5c1d094c18e240de18ed363398615d5f8a969dc7
Glauber Brack <glauber@brack.com.br>
2020-08-14 14:05:55 -0300

    created migration, model and repository for Product; and start of ProducController

---
+++ src/database/migrations/1597424569357-CreateProducts.ts
@@ -0,0 +1,40 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { query } from "express";
+
+export class CreateProducts1597424569357 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'products',
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                        generationStrategy: "uuid",
+                        default: "uuid_generate_v4()",
+                      },
+                      {
+                          name: 'name',
+                          type: 'varchar'
+                      },
+                      {
+                          name: 'description',
+                          type: 'varchar'
+                      },
+                      {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                      },
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('products');
+    }
+
+}
---
+++ src/models/Product.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity('products')
+class Product {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+    
+}
+
+export default Product;
