99b222ec0d48bc35c260fd914739726b35c0345a
Glauber Brack <glauber@brack.com.br>
2020-08-14 12:47:59 -0300

    Making User receive role

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,13 +3,15 @@ import { getCustomRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
 import UserRepository from '../repositories/UserRepository';
+import RoleRepository from '../repositories/RoleRepository';
 
 class UserController {
 
     async store(request: Request, response: Response) {
         const userRepository = getCustomRepository(UserRepository);
+        const roleRepository = getCustomRepository(RoleRepository);
 
-        const { name, username, password } = request.body;
+        const { name, username, password, roles } = request.body;
 
         const userExists = await userRepository.findOne({ username });
 
@@ -19,10 +21,13 @@ class UserController {
 
         const passwordHashed = await hash(password, 8);
 
+        const roleExists = await roleRepository.findByIds(roles);
+
         const user = userRepository.create({
             name,
             username,
-            password: passwordHashed
+            password: passwordHashed,
+            role: roleExists
         });
 
         await userRepository.save(user);
